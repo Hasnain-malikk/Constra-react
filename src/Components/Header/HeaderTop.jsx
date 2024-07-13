b25c49fdb12c0bce2e62007031311c49380e9698
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-13 12:43:15 +0500

    Header top completed

--- src/Components/Header/HeaderTop.jsx
+++ src/Components/Header/HeaderTop.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "./HeaderTop.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+
 
 const HeaderTop = () => {
   return (
     <div className='header-top'>
-      this is me top
+      <div className="address">
+      <FontAwesomeIcon icon={faLocationDot} style={{margin:"10px",fontSize:"18px"}}/>
+      <div>9051 Constra Incorporate, USA</div>
+      </div>
+      <div className="social">
+        <a href="https://web.facebook.com/malik.husnain.1612/" target='_blank'><FontAwesomeIcon icon={faFacebookF} style={{margin:"15px" }}/></a>
+        <a href="#"><FontAwesomeIcon icon={faTwitter} style={{margin:"15px"}}/></a>
+        <a href="https://www.instagram.com/malikhasnaindev/" target='_blank'><FontAwesomeIcon icon={faInstagram} style={{margin:"15px"}}/></a>
+        <a href="https://github.com/Hasnain-malikk" target='_blank'><FontAwesomeIcon icon={faGithub} style={{margin:"15px"}}/></a>
+      </div>
     </div>
   )
 }
