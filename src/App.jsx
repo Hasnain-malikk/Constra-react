5ec4f2222c989d9b1cd65fe1a2074797008409bb
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-13 17:58:16 +0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderTop from './Components/Header/HeaderTop'
 import HeaderBottom from './Components/Header/HeaderBottom'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
     <div>
       <HeaderTop/>
       <HeaderBottom/>
+      <Navbar/>
     </div>
   )
 }
