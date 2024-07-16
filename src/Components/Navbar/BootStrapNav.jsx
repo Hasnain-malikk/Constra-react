8dcb1176669eb92797cb5874722b70b1da181793
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-16 20:40:41 +0500

    modified

--- src/Components/Navbar/BootStrapNav.jsx
+++ src/Components/Navbar/BootStrapNav.jsx
@@ -9,25 +9,19 @@ function BootStrapNav() {
     <Navbar expand="lg" style={{backgroundColor:"#23282D",height:"75px"}} className="bg-body-pink">
       <Container>
         <Navbar.Toggle aria-controls="basic-navbar-nav" style={{backgroundColor:"#FFB600",border:"0",color:"#23282D"}} />
-        <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav" className="custom-collapse">
           <Nav className="me-auto" id='mdq'>
             <Nav.Link href="#home" id='navLinks' className="nav-link-custom">HOME</Nav.Link>
             <NavDropdown style={{color:"white",fontFamily:"Montserrat",fontSize:"14px",fontWeight:"700"}} title="COMPANY" id="basic-nav-dropdown">
               <NavDropdown.Item id='new' href="#action/3.1">ABOUT US</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item id='new' href="#action/3.2">
-                OUR PEOPLE
-              </NavDropdown.Item>
+              <NavDropdown.Item id='new' href="#action/3.2">OUR PEOPLE</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item id='new' href="#action/3.3">TESTIMONIALS</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item id='new' href="#action/3.4">
-                FAQ
-              </NavDropdown.Item>
+              <NavDropdown.Item id='new' href="#action/3.4">FAQ</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item id='new' href="#action/3.4">
-                PRICING
-              </NavDropdown.Item>
+              <NavDropdown.Item id='new' href="#action/3.4">PRICING</NavDropdown.Item>
             </NavDropdown>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">PROJECTS</Nav.Link>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">SERVICES</Nav.Link>
