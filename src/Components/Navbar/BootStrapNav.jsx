01d14222f1ea8156a21619804edf526a013d5418
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-16 19:25:11 +0500

    navbar completed

--- src/Components/Navbar/BootStrapNav.jsx
+++ src/Components/Navbar/BootStrapNav.jsx
@@ -13,21 +13,27 @@ function BootStrapNav() {
           <Nav className="me-auto" id='mdq'>
             <Nav.Link href="#home" id='navLinks' className="nav-link-custom">HOME</Nav.Link>
             <NavDropdown style={{color:"white",fontFamily:"Montserrat",fontSize:"14px",fontWeight:"700"}} title="COMPANY" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item id='new' href="#action/3.1">ABOUT US</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item id='new' href="#action/3.2">
+                OUR PEOPLE
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item id='new' href="#action/3.3">TESTIMONIALS</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item id='new' href="#action/3.4">
+                FAQ
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item id='new' href="#action/3.4">
+                PRICING
               </NavDropdown.Item>
             </NavDropdown>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">PROJECTS</Nav.Link>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">SERVICES</Nav.Link>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">FEATURES</Nav.Link>
             <Nav.Link id='navLinks' className="nav-link-custom" href="#link">NEWS</Nav.Link>
-            <Nav.Link id='navLinks' className="nav-link-custom" style={{marginBottom:"15px"}} href="#link">CONTACT</Nav.Link>
+            <Nav.Link id='navLinks' className="nav-link-custom" href="#link">CONTACT</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
