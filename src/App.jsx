16031dc2226fb84dd7a2c808b453cdf8535385f2
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-18 10:54:04 +0500

    react router added

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,42 @@ import HeaderBottom from './Components/Header/HeaderBottom'
 import BootStrapNav from './Components/Navbar/BootStrapNav'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Components/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import AboutUs from './Pages/AboutUs';
+import OurPeople from './Pages/OurPeople';
+import Testimonials from './Pages/Testimonials';
+import FAQ from './Pages/FAQ';
+import Pricing from './Pages/Pricing';
+import Projects from './Pages/Projects';
+import Services from './Pages/Services';
+import Features from './Pages/Features';
+import News from './Pages/News';
+import Contact from './Pages/Contact';
 
 
 const App = () => {
   return (
     <div>
-      <HeaderTop/>
-      <HeaderBottom/>
-      <BootStrapNav/>
-      <Footer/>
+      <BrowserRouter>
+       <HeaderTop/>
+       <HeaderBottom/>
+       <BootStrapNav/>
+        <Routes>
+         <Route path="/" element={<Home/>} /> 
+         <Route path="/aboutus" element={<AboutUs/>} /> 
+         <Route path="/ourpeople" element={<OurPeople/>} /> 
+         <Route path="/testimonials" element={<Testimonials/>} /> 
+         <Route path="/faq" element={<FAQ/>} /> 
+         <Route path="/pricing" element={<Pricing/>} /> 
+         <Route path="/projects" element={<Projects/>} /> 
+         <Route path="/services" element={<Services/>} /> 
+         <Route path="/features" element={<Features/>} /> 
+         <Route path="/news" element={<News/>} /> 
+         <Route path="/contact" element={<Contact/>} /> 
+        </Routes>
+       <Footer/>
+      </BrowserRouter>
     </div>
   )
 }
