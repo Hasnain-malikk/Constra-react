7b4d022a803872ad9324eb9cfecf56f788027b35
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-19 20:07:24 +0500

    section 2 left completed

---
+++ src/Components/Section2/Section2.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import "./Section2.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrophy, faSliders, faThumbsUp, faUsers } from '@fortawesome/free-solid-svg-icons';
+
+const Section2 = () => {
+  return (
+    <div className='section-2'>
+      <div className="section-2-left">
+        <div className="section-2-left-1">ABOUT US</div>
+        <div className="section-2-left-2">WE DELIVER LANDMARK PROJECTS</div>
+        <div className="section-2-left-3">We are rethoric question ran over her cheek When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.
+        </div>
+        <div className="section-2-left-4">
+            <div className="section-2-left-4-box">
+                <div className="box-logo">
+                <FontAwesomeIcon icon={faTrophy} />
+                </div>
+                <div className="box-text">WE'VE REPUTION FOR EXCELLENCE</div>
+            </div>
+            <div className="section-2-left-4-box">
+            <div className="box-logo">
+                <FontAwesomeIcon icon={faSliders} />
+                </div>
+                <div className="box-text">WE BUILD PARTNERSHIPS</div>
+            </div>
+            <div className="section-2-left-4-box">
+            <div className="box-logo">
+                <FontAwesomeIcon icon={faThumbsUp} />
+                </div>
+                <div className="box-text">GUIDED BY COMMITMENT</div>
+            </div>
+            <div className="section-2-left-4-box">
+            <div className="box-logo">
+                <FontAwesomeIcon icon={faUsers} />
+                </div>
+                <div className="box-text">A TEAM OF PROFESSIONALS</div>
+            </div>
+        </div>
+      </div>
+      <div className="section-2-right"></div>
+    </div>
+  )
+}
+
+export default Section2
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import SwiperCarousel from '../Components/SwiperCarousel/SwiperCarousel'
+import Section2 from '../Components/Section2/Section2'
 
 
 const Home = () => {
   return (
     <div>
       <SwiperCarousel/>
+      <Section2/>
     </div>
   )
 }
