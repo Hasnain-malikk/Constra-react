25738880e36495fad732031217a8838d075c770c
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-21 22:56:01 +0500

    links added

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -11,12 +11,12 @@ const Projects = () => {
       </div>
       <div className="projects-nav">
         <Link to="/" className='project-link'>SHOW ALL</Link>
-        <Link to="/" className='project-link'>COMMERCIAL</Link>
-        <Link to="/" className='project-link'>EDUACTION</Link>
-        <Link to="/" className='project-link'>GOVERNMENT</Link>
-        <Link to="/" className='project-link'>INFRASTRUCTURE</Link>
-        <Link to="/" className='project-link'>RESIDENTIAL</Link>
-        <Link to="/" className='project-link'>HEALTHCARE</Link>
+        <Link to="/commercial" className='project-link'>COMMERCIAL</Link>
+        <Link to="/eduaction" className='project-link'>EDUACTION</Link>
+        <Link to="/government" className='project-link'>GOVERNMENT</Link>
+        <Link to="/infrastructure" className='project-link'>INFRASTRUCTURE</Link>
+        <Link to="/residential" className='project-link'>RESIDENTIAL</Link>
+        <Link to="/healthcare" className='project-link'>HEALTHCARE</Link>
       </div>
     </div>
   )
