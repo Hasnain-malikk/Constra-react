1447e8ecb90e4d9d8524c92bcf23727a03a4dcf3
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-21 12:53:16 +0500

    counter completed

--- src/Components/Counter/Count.jsx
+++ src/Components/Counter/Count.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import "./Count.css"
-import count1 from "/ReactJs/constra-react/src/assets/counter-house.png"
+import React from 'react';
+import "./Count.css";
+import CountUp from 'react-countup';
+import { useInView } from 'react-intersection-observer';
 
-const Count = () => {
+const Count = ({ icon, number, text }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true, // Trigger only once
+    threshold: 0.1, // Trigger when 10% of the component is in view
+  });
+
   return (
-    <div className='count'>
+    <div className='count' ref={ref}>
       <div className="count-icon">
-        <img src={count1} alt="" />
+        <img src={icon} alt="" />
       </div>
-      <div className="count-up">1786</div>
-      <div className="count-text">TOTAL PROJECTS</div>
+      <div className="count-up">
+        {inView ? <CountUp end={number} duration={4} separator="" /> : null}
+      </div>
+      <div className="count-text">{text}</div>
     </div>
-  )
+  );
 }
 
-export default Count
+export default Count;
--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import "./Counter.css"
 import Count from './Count'
+import count1 from "/ReactJs/constra-react/src/assets/counter-house.png"
+import count2 from "/ReactJs/constra-react/src/assets/counter-person.png"
+import count3 from "/ReactJs/constra-react/src/assets/counter-clock.png"
+import count4 from "/ReactJs/constra-react/src/assets/counter-globe.png"
 
 const Counter = () => {
   return (
     <div className='counter'>
-      <Count/>
-      <Count/>
-      <Count/>
-      <Count/>
+      <Count icon={count1} number="1789" text="TOTAL PROJECTS"/>
+      <Count icon={count2} number="647" text="STAFF MEMBERS"/>
+      <Count icon={count3} number="4000" text="HOURS OF WORK"/>
+      <Count icon={count4} number="44" text="COUNTRIES EXPERIENCE"/>
     </div>
   )
 }
