558588f8e92d883caeae7d5029921ba599b02b74
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-25 18:48:15 +0500

    counter added in about

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import WhoWeAre from '../Components/WhoWeAre/WhoWeAre'
 import AboutHeader from '../Components/AboutHeader/AboutHeader'
+import Counter from '../Components/Counter/Counter'
 
 const AboutUs = () => {
   return (
     <div>
       <AboutHeader/>
       <WhoWeAre/>
+      <Counter/>
     </div>
   )
 }
