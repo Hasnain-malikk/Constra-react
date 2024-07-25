64cba54b027f090c9eae6052394eb144f610093f
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-25 22:14:40 +0500

    Team section completed

--- src/Components/PersonalTeam/PersonalTeam.jsx
+++ src/Components/PersonalTeam/PersonalTeam.jsx
@@ -4,6 +4,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation} from 'swiper/modules';
 import 'swiper/css/navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const PersonalTeam = () => {
   return (
@@ -21,13 +27,86 @@ const PersonalTeam = () => {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
         >
-        <SwiperSlide className='swiper4-slide'>Slide 1</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 2</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 3</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 4</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 4</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 4</SwiperSlide>
-        <SwiperSlide className='swiper4-slide'>Slide 4</SwiperSlide>
+        <SwiperSlide id='pt-s1' className='swiper4-slide'>
+        <div className="pt-content">
+            <div className="pt-content-1">NATS STENMAN</div>
+            <div className="pt-content-2">Chief Operating Officer</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide id='pt-s2' className='swiper4-slide'>
+        <div className="pt-content">
+            <div className="pt-content-1">ANGELA LYOUER</div>
+            <div className="pt-content-2">Innovation Officer</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide id='pt-s3' className='swiper4-slide'>
+            <div className="pt-content">
+            <div className="pt-content-1">MARK CONTER</div>
+            <div className="pt-content-2">Safety Officer</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide id='pt-s4' className='swiper4-slide'>
+        <div className="pt-content">
+            <div className="pt-content-1">AYESHA STEWART</div>
+            <div className="pt-content-2">Finance Officer</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide id='pt-s5' className='swiper4-slide'>
+        <div className="pt-content">
+            <div className="pt-content-1">DAVE CLARKTE</div>
+            <div className="pt-content-2">Civil Engineer</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide id='pt-s6' className='swiper4-slide'>
+        <div className="pt-content">
+            <div className="pt-content-1">ELTON JOE</div>
+            <div className="pt-content-2">Site Supervisor</div>
+            <div className="pt-content-3">Nats Stenman began his career in construction with boots on the ground</div>
+            <div className="pt-content-4">
+                <FontAwesomeIcon className='pt-icon' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon' icon={faLinkedin} />
+            </div>
+            </div>
+        </SwiperSlide>
+        
+
         </Swiper>
       </div>
     </div>
