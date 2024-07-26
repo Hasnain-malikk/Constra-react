44b158847d5f8a6dffc1b203d200b4a679f4aed5
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-26 12:23:20 +0500

    OUR LEADERSHIP component mounted to OUR PEOPLE

---
+++ src/Components/OurLeadership/OurLeadership.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./OurLeadership.css"
+
+const OurLeadership = () => {
+  return (
+    <div className='our-leadership'>
+      this is out leadership
+    </div>
+  )
+}
+
+export default OurLeadership
--- src/Pages/OurPeople.jsx
+++ src/Pages/OurPeople.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AboutHeader from '../Components/AboutHeader/AboutHeader'
+import OurLeadership from '../Components/OurLeadership/OurLeadership'
 
 const OurPeople = () => {
   return (
     <div>
       <AboutHeader heading1="OUR TEAM" heading2="HOME / COMPANY / OUR TEAM"/>
+      <OurLeadership/>
     </div>
   )
 }
