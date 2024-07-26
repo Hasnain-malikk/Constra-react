abc96faedd64a3a6422994047c279371aa7b5651
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-26 17:20:07 +0500

    Our People completed

--- src/Components/OurLeadership/OurLeadership.jsx
+++ src/Components/OurLeadership/OurLeadership.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import "./OurLeadership.css"
 import People from './People'
+import p1 from "/ReactJs/constra-react/src/assets/team1.jpg"
+import p2 from "/ReactJs/constra-react/src/assets/team2.jpg"
+import p3 from "/ReactJs/constra-react/src/assets/team3.jpg"
+import p4 from "/ReactJs/constra-react/src/assets/team4.jpg"
+import p5 from "/ReactJs/constra-react/src/assets/team5.jpg"
+import p6 from "/ReactJs/constra-react/src/assets/team6.jpg"
+import p7 from "/ReactJs/constra-react/src/assets/team7.jpg"
+import p8 from "/ReactJs/constra-react/src/assets/team8.jpg"
 
 const OurLeadership = () => {
   return (
     <div className='our-leadership'>
       <div className="our-leadership-heading">OUR LEADERSHIPS</div>
       <div className="our-leadership-body">
-        <People/>
+        <People img={p1} name="NATS STENMAN" desc="Chief Operating Officer" />
+        <People img={p2} name="ANGELA LYOUER" desc="Innovation Officer" />
+        <People img={p3} name="MARK CONTER" desc="Safety Officer" />
+        <People img={p4} name="AYESHA STEWART" desc="Finance Officer" />
+        <People img={p8} name="JHON SNOW" desc="Site Engineer" />
+        <People img={p5} name="DAVE CLARKTE" desc="Civil Engineer" />
+        <People img={p6} name="ELTON JOE" desc="Site Supervisor" />
+        <People img={p7} name="DANERYAS TARGARYEIN" desc="Project Manager" />
       </div>
     </div>
   )
--- src/Components/OurLeadership/People.jsx
+++ src/Components/OurLeadership/People.jsx
@@ -7,15 +7,15 @@ import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-const People = () => {
+const People = ({img,name,desc}) => {
   return (
     <div className='people'> 
       <div className="people-img">
-        <img src={p1} alt="" />
+        <img src={img} alt="" />
       </div>
       <div className="people-body">
-        <div className="people-body-1">NATS STENMAN</div>
-        <div className="people-body-2">Chief Operating Officer</div>
+        <div className="people-body-1">{name}</div>
+        <div className="people-body-2">{desc}</div>
         <div className="people-body-3">Nats Stenman began his career in construction with boots on the ground</div>
         <div className="people-body-4">
                 <FontAwesomeIcon className='pt-icon dark' icon={faFacebookF} />
