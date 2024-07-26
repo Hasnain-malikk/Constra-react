dbbfbcccba583c6ef60340bad854f85fe7b5df01
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-26 12:56:51 +0500

    People card completed

--- src/Components/OurLeadership/OurLeadership.jsx
+++ src/Components/OurLeadership/OurLeadership.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./OurLeadership.css"
+import People from './People'
 
 const OurLeadership = () => {
   return (
     <div className='our-leadership'>
-      this is out leadership
+      <div className="our-leadership-heading">OUR LEADERSHIPS</div>
+      <div className="our-leadership-body">
+        <People/>
+      </div>
     </div>
   )
 }
---
+++ src/Components/OurLeadership/People.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./People.css"
+import p1 from "/ReactJs/constra-react/src/assets/team1.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGooglePlus } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const People = () => {
+  return (
+    <div className='people'> 
+      <div className="people-img">
+        <img src={p1} alt="" />
+      </div>
+      <div className="people-body">
+        <div className="people-body-1">NATS STENMAN</div>
+        <div className="people-body-2">Chief Operating Officer</div>
+        <div className="people-body-3">Nats Stenman began his career in construction with boots on the ground</div>
+        <div className="people-body-4">
+                <FontAwesomeIcon className='pt-icon dark' icon={faFacebookF} />
+                <FontAwesomeIcon className='pt-icon dark' icon={faTwitter} />
+                <FontAwesomeIcon className='pt-icon dark' icon={faGooglePlus} />
+                <FontAwesomeIcon className='pt-icon dark' icon={faLinkedin} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default People
