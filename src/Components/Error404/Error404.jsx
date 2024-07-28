71a82287dd938cfe7ef84061e2d2ccaa549ca0ad
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-28 20:58:56 +0500

    Page not found added

---
+++ src/Components/Error404/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Error404.css"
+
+const Error404 = () => {
+  return (
+    <div className='error-404'>
+      404 error page not found
+    </div>
+  )
+}
+
+export default Error404
--- src/Pages/Features.jsx
+++ src/Pages/Features.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AboutHeader from '../Components/AboutHeader/AboutHeader'
+import Error404 from '../Components/Error404/Error404'
 
 const Features = () => {
   return (
     <div>
-      this is Features
+      <AboutHeader heading1 = "FEATURES" heading2 = "HOME / COMPANY / 404" />
+      <Error404/>
     </div>
   )
 }
