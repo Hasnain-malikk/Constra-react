9e1749daa21279c8cc340c7eea260dedb6b54981
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-28 21:13:23 +0500

    features completed

--- src/Components/Error404/Error404.jsx
+++ src/Components/Error404/Error404.jsx
@@ -4,7 +4,12 @@ import "./Error404.css"
 const Error404 = () => {
   return (
     <div className='error-404'>
-      404 error page not found
+      <div className="error-main1">404</div>
+      <div className="error-main2">OOPS... PAGE NOT FOUND!</div>
+      <div className="error-main3">Try using the button below to go to main page of the site</div>
+      <div className="error-main4">
+        <button>BACK TO HOME PAGE</button>
+      </div>
     </div>
   )
 }
