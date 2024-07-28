effa0860827936c0caa2639feef8354228a11f84
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-28 16:20:49 +0500

    Services page completed

--- src/Components/ServicesBody/ServiceMain.jsx
+++ src/Components/ServicesBody/ServiceMain.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./ServiceMain.css"
+import WWD1 from '../WhatWeDo/WWD1'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
-const ServiceMain = () => {
+const ServiceMain = ({icn,head,img}) => {
   return (
     <div className='service-main'>
-      this is main service
+      <div className="service-img">
+        <img src={img} alt="" />
+      </div>
+      <WWD1 icon={icn} heading={head} text="You have ideas, goals, and dreams. We have a culturally diverse, forward thinking team looking for talent like. Lorem ipsum dolor suscipit." />
+      <div className="service-main-link">
+      <FontAwesomeIcon icon={faCaretRight} />
+      &nbsp;LEARN MORE
+      </div>
     </div>
   )
 }
--- src/Components/ServicesBody/ServicesBody.jsx
+++ src/Components/ServicesBody/ServicesBody.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import "./ServicesBody.css"
 import ServiceMain from './ServiceMain'
+import p1 from "/ReactJs/constra-react/src/assets/service1.jpg"
+import p2 from "/ReactJs/constra-react/src/assets/service2.jpg"
+import p3 from "/ReactJs/constra-react/src/assets/service3.jpg"
+import p4 from "/ReactJs/constra-react/src/assets/service4.jpg"
+import p5 from "/ReactJs/constra-react/src/assets/service5.jpg"
+import p6 from "/ReactJs/constra-react/src/assets/service6.jpg"
+import i1 from "/ReactJs/constra-react/src/assets/constra-1.png"
+import i2 from "/ReactJs/constra-react/src/assets/constra-2.png"
+import i3 from "/ReactJs/constra-react/src/assets/constra-3.png"
+import i4 from "/ReactJs/constra-react/src/assets/constra-4.png"
+import i5 from "/ReactJs/constra-react/src/assets/constra-5.png"
+import i6 from "/ReactJs/constra-react/src/assets/constra-6.png"
 
 const ServicesBody = () => {
   return (
     <div className='services-body'>
-      <ServiceMain/>
+      <ServiceMain img={p1} icn={i1}  head="ZERO HARM EVERYDAY"  />
+      <ServiceMain img={p2} icn={i2}  head="VIRTUAL CONSTRUCTION"  />
+      <ServiceMain img={p3} icn={i3}  head="BUILD TO LAST"  />
+      <ServiceMain img={p4} icn={i4}  head="EXTERIOR DESIGN"  />
+      <ServiceMain img={p5} icn={i5}  head="RENOVATION"  />
+      <ServiceMain img={p6} icn={i6}  head="SAFETY MANAGEMENT"  />
     </div>
   )
 }
