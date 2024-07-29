dfc117fc52ba5ae62ac340a5a63bf1e46d430d59
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-29 14:30:38 +0500

    news page updated

--- src/Components/NewsBody/NewsBody.jsx
+++ src/Components/NewsBody/NewsBody.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import "./NewsBody.css"
 import p1 from "/ReactJs/constra-react/src/assets/rp-1.jpg"
 import p2 from  "/ReactJs/constra-react/src/assets/testimonial-1.png"
+import p3 from  "/ReactJs/constra-react/src/assets/testimonial-2.png"
+import p4 from  "/ReactJs/constra-react/src/assets/testimonial-3.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faFolderOpen, faCalendar, faComment } from '@fortawesome/free-regular-svg-icons';
 import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF , faGooglePlus, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons';
+import RpSide from '../RpSide/RpSide'
+
 
 const NewsBody = () => {
   return (
@@ -16,17 +20,17 @@ const NewsBody = () => {
         </div>
         <div className="news-body1-2">
             <div className="post-meta">
-               <FontAwesomeIcon icon={faUser} />
+               <FontAwesomeIcon className='metaIcn' icon={faUser} />
                <div className='post-meta1'>&nbsp;&nbsp;Admin&nbsp;&nbsp;&nbsp;</div>
-               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faFolderOpen} />
+               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon className='metaIcn' icon={faFolderOpen} />
                <div className='post-meta1'>&nbsp;&nbsp;News&nbsp;&nbsp;&nbsp;</div>
-               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faCalendar} />
+               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon className='metaIcn' icon={faCalendar} />
                <div className='post-meta1'>&nbsp;&nbsp;July 28, 2024&nbsp;&nbsp;&nbsp;</div>
-               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon icon={faComment} />
+               &nbsp;&nbsp;&nbsp;<FontAwesomeIcon className='metaIcn' icon={faComment} />
                <div className='post-meta1 last'>&nbsp;&nbsp;03 Comments&nbsp;&nbsp;&nbsp;</div>
             </div>
             <div className="entry-heading">
-            We Just Completes $17.6 million Medical Clinic in Mid-Missouri
+            We Just Completes $17.6 Million Medical Clinic In Mid-Missouri
             </div>
             <div className="news-body-para1">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -86,12 +90,78 @@ const NewsBody = () => {
                         <div className="comment-head">Michelle Aimber</div>
                         <div className="comment-head-end">January 17, 2016 at 1:38 pm</div>
                         </div>
+                        <div className="news-body-para1">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehen.
+                        </div>
+                        <div className="reply">Reply</div>
                     </div>
+                </div>
+                <div className="comment-body rpl">
+                    <div className="comment-body-left">
+                        <img src={p3} alt="" />
+                    </div>
+                    <div className="comment-body-right">
+                        <div className="comment-body-right1">
+                        <div className="comment-head">Tom Harnandez</div>
+                        <div className="comment-head-end">January 17, 2016 at 1:38 pm</div>
+                        </div>
+                        <div className="news-body-para1">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehen.
+                        </div>
+                        <div className="reply">Reply</div>
+                    </div>
+                </div>
+                <div className="comment-body">
+                    <div className="comment-body-left">
+                        <img src={p4} alt="" />
+                    </div>
+                    <div className="comment-body-right">
+                        <div className="comment-body-right1">
+                        <div className="comment-head">Genelia Dusteen</div>
+                        <div className="comment-head-end">January 17, 2016 at 1:38 pm</div>
+                        </div>
+                        <div className="news-body-para1">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehen.
+                        </div>
+                        <div className="reply">Reply</div>
+                    </div>
+                </div>
+                <div className='news-body-form'>
+                    <div className="news-form1">ADD A COMMENT</div>
+                    <textarea className='nbftf news-body-form-1' placeholder='Your Comment' name="dwoo" id=""></textarea>
+                    <input placeholder='Your Name' className='news-body-form-1' type="text" />
+                    <input placeholder='Your Email' className='news-body-form-1 mr' type="text" />
+                    <input placeholder='Your Website' className='news-body-form-1' type="text" />
+                    <button className='pricing-main-button clro' >POST  COMMENT</button>
                 </div>
             </div>
         </div>
       </div>
-      <div className="news-body2">ss</div>
+      <div className="news-body2">
+        <div className="news-body2-1">
+            <RpSide/>
+            <div className="news-body2-2">
+                <div className="categorey">
+                CATEGORIES
+                </div>
+                <div className="tag1">Construction</div>
+                <div className="tag1">Commericial</div>
+                <div className="tag1">Building</div>
+                <div className="tag1">Safety</div>
+                <div className="tag1 tag2">Structure</div>
+            </div>
+            <div className="news-body2-2">
+                <div className="categorey">
+                ARCHIVES
+                </div>
+                <div className="tag1">Feburay 2016</div>
+                <div className="tag1">January 2016</div>
+                <div className="tag1">December 2015</div>
+                <div className="tag1">November 2015</div>
+                <div className="tag1 tag2">October 2015</div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
