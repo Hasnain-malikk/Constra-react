b7619bd050da93d215b9d726e025a952fe1c7e29
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-30 22:06:59 +0500

    single project added

--- src/Components/ProjectBody/ProjectBody.jsx
+++ src/Components/ProjectBody/ProjectBody.jsx
@@ -1,21 +1,58 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import "./ProjectBody.css"
+import React from 'react';
+import { Link, useLocation } from "react-router-dom";
+import "./ProjectBody.css";
 
 const ProjectBody = () => {
+  const location = useLocation();
+
   return (
     <div className='project-body'>
       <div className="project-bar">
-        <Link className="p-link" to='showall' >SHOW ALL</Link>
-        <Link className="p-link" to='commercial' >COMMERCIAL</Link>
-        <Link className="p-link" to='education' >EDUCATION</Link>
-        <Link className="p-link" to='government' >GOVERNMENT</Link>
-        <Link className="p-link" to='infrastructure' >INFRASTRUCTURE</Link>
-        <Link className="p-link" to='residential' >RESIDENTIAL</Link>
-        <Link className="p-link" to='healthcare' >HEALTHCARE</Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/showall' ? 'active' : ''}`} 
+          to='showall'
+        >
+          SHOW ALL
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/commercial' ? 'active' : ''}`} 
+          to='commercial'
+        >
+          COMMERCIAL
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/education' ? 'active' : ''}`} 
+          to='education'
+        >
+          EDUCATION
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/government' ? 'active' : ''}`} 
+          to='government'
+        >
+          GOVERNMENT
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/infrastructure' ? 'active' : ''}`} 
+          to='infrastructure'
+        >
+          INFRASTRUCTURE
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/residential' ? 'active' : ''}`} 
+          to='residential'
+        >
+          RESIDENTIAL
+        </Link>
+        <Link 
+          className={`p-link ${location.pathname === '/projects/healthcare' ? 'active' : ''}`} 
+          to='healthcare'
+        >
+          HEALTHCARE
+        </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProjectBody
+export default ProjectBody;
---
+++ src/Components/ProjectSingle/ProjectSingle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectSingle = () => {
+  return (
+    <div className='project-single'>
+      this si single project
+    </div>
+  )
+}
+
+export default ProjectSingle
