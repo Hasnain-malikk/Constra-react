522c16e057191dbea4a4600c7ed0f79392dcdc81
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-31 12:38:37 +0500

    mapping added to project-single

--- src/Components/ProjectPages/ShowAll.jsx
+++ src/Components/ProjectPages/ShowAll.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ProjectSingle from '../ProjectSingle/ProjectSingle'
+import p1 from "/ReactJs/constra-react/src/assets/project1.jpg"
 
 const ShowAll = () => {
   return (
     <div className='project-page-body'>
-      <ProjectSingle/>
-      <ProjectSingle/>
-      <ProjectSingle/>
+      <ProjectSingle bgimg="bg1"  name="CAPITAL TELTWAY BUILDING" type="COMMERCIAL INTERIORS" img={p1}/>
+      
     </div>
   )
 }
--- src/Components/ProjectSingle/ProjectSingle.jsx
+++ src/Components/ProjectSingle/ProjectSingle.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import "./ProjectSingle.css";
-import p1 from "/ReactJs/constra-react/src/assets/project1.jpg"
 
-const ProjectSingle = () => {
+const ProjectSingle = ({bgimg,img,name,type}) => {
   const [isSheetVisible, setIsSheetVisible] = useState(false);
 
   const handleButtonClick = () => {
@@ -14,20 +13,20 @@ const ProjectSingle = () => {
   };
 
   return (
-    <div className='project-single'>
+    <div className='project-single' id={bgimg} >
       <div className="front">
         <div className="plus-btn">
           <button onClick={handleButtonClick}>+</button>
         </div>
         <div className="project-name">
-          CAPITAL TELTWAY BUILDING
+          {name}
         </div>
-        <span className='project-type'>COMMERCIAL INTERIORS</span>
+        <span className='project-type'>{type}</span>
       </div>
       {isSheetVisible && (
         <div className={`sheet ${isSheetVisible ? 'active' : ''}`} onClick={handleCloseSheet}>
           <div className="sheet-content">
-            <img src={p1} alt="" />
+            <img src={img} alt="" />
           </div>
         </div>
       )}
