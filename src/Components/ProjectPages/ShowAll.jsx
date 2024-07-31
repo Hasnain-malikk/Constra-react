c72b4b6ffdc8e86dec20cb63525d1d9c4ee80d5a
Hasnain-malikk <139120531+Hasnain-malikk@users.noreply.github.com>
2024-07-31 12:54:57 +0500

    project-single responsive added

--- src/Components/ProjectPages/ShowAll.jsx
+++ src/Components/ProjectPages/ShowAll.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ProjectSingle from '../ProjectSingle/ProjectSingle'
 import p1 from "/ReactJs/constra-react/src/assets/project1.jpg"
+import p2 from "/ReactJs/constra-react/src/assets/project2.jpg"
+import p3 from "/ReactJs/constra-react/src/assets/project3.jpg"
+import p4 from "/ReactJs/constra-react/src/assets/project4.jpg"
+import p5 from "/ReactJs/constra-react/src/assets/project5.jpg"
+import p6 from "/ReactJs/constra-react/src/assets/project6.jpg"
 
 const ShowAll = () => {
   return (
     <div className='project-page-body'>
       <ProjectSingle bgimg="bg1"  name="CAPITAL TELTWAY BUILDING" type="COMMERCIAL INTERIORS" img={p1}/>
-      
+      <ProjectSingle bgimg="bg2"  name="GHUM TOUCH HOSPITAL" type="HEALTHCARE" img={p2}/>
+      <ProjectSingle bgimg="bg3"  name="TNT EAST FACILITY" type="GOVERNMENT" img={p3}/>
+      <ProjectSingle bgimg="bg4"  name="NARRIOT HEADQUARTERS" type="INFRASTRUCTURE" img={p4}/>
+      <ProjectSingle bgimg="bg5"  name="KALAS METRORAIL" type="INFRASTRUCTURE" img={p5}/>
+      <ProjectSingle bgimg="bg6"  name="ANCRAFT AVENUE HOUSE" type="Residential" img={p6}/>
     </div>
   )
 }
